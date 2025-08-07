63059dc5f7a7e4ba5c962b0518775289f77e67af
houssamos <touilhoussem@yahoo.fr>
2025-08-07 22:41:51 +0200

    Add protected route component and guard pages

--- app/carte/page.tsx
+++ app/carte/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import CarteComponent from '@/components/pages/carte/CarteComponent';
+import ProtectedRoute from '@/components/auth/ProtectedRoute';
 
 export default function CartePage() {
   return (
-    <main className="h-screen">
-      <CarteComponent />
-    </main>
+    <ProtectedRoute>
+      <main className="h-screen">
+        <CarteComponent />
+      </main>
+    </ProtectedRoute>
   );
 }
